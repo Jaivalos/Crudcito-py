2167c11ecfa5106024db98439bbc186ea86d89c7
Javier Avalos <javalos@ccg.gt>
2023-02-13 16:48:22 -0600

    Conexion y menu creados

---
+++ BD/conexion.py
@@ -0,0 +1,27 @@
+import mysql.connector
+from mysql.connector import Error
+
+class DAO():
+    def __init__(self):
+        try:
+            self.conexion=mysql.connector.connect(
+                host="localhost",
+                port=3306,
+                user="root",
+                password="javi0712",
+                db="crud-basico"
+            )
+        except Error as e:
+            print("Error al intentar la conexion ", e)
+
+    def obtener_cursos(self):
+        if self.conexion.is_connected():
+            try:
+                cursor = self.conexion.cursor()
+                cursor.execute("SELECT * FROM curso ORDER BY nombre ASC")
+                resultados = cursor.fetchall()
+                return resultados
+            except Error as e:
+                print("Error al obtener cursos ", e)
+
+                
---
+++ main.py
@@ -0,0 +1,33 @@
+import sys
+
+def menu():
+
+    #Creacion de menu de opciones
+    dato = True
+    while (dato):
+        opcion_correcta = False
+        while (not opcion_correcta):
+            print("---------------- Menú Principal ----------------")
+            print("1. Listar Cursos")
+            print("2. Añadir Curso")
+            print("3. Actualizar Curso")
+            print("4. Eliminar Curso")
+            print("5. Salir")
+
+            opcion = int(input("\n Selecciona una opción: "))
+
+            if (opcion < 1) or (opcion > 5):
+                print("Dato incorrecto, seleccione una opcion valida")
+            elif (opcion == 5): 
+                dato = False
+                print("Buen día") 
+                break
+            else:
+                ejecutar_opcion(opcion)
+
+def ejecutar_opcion(opcion):
+    print(opcion)
+
+
+menu()  
+
